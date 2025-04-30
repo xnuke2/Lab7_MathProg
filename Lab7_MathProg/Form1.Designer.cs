e1e0e228279f9fd57065124beea95c63a0f1c08a
xnuke2 <xmnikit@gmail.com>
2025-04-30 05:01:23 +0300

    Добавьте файлы проекта.

---
+++ Lab7_MathProg/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace Lab7_MathProg
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ColumnHeadersVisible = false;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 35);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(341, 154);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(61, 7);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(118, 20);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ранг матрицы";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(217, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 22);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Заполнить случайными";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.ColumnHeadersVisible = false;
+            this.dataGridView2.Location = new System.Drawing.Point(10, 232);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersVisible = false;
+            this.dataGridView2.Size = new System.Drawing.Size(341, 154);
+            this.dataGridView2.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(231, 194);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 32);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Решить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 406);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "значение";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(368, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Lab7_MathProg/Form1.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab7_MathProg
+{
+    public partial class Form1 : Form
+    {
+        int range;
+        int rowIndexGlobal = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            range = Convert.ToInt32(numericUpDown1.Value);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            numericUpDown1_ValueChanged(sender, e);
+            int[,] init =
+            {
+                {47,53, 67 },
+                {35,7,52 },
+                {96,35,54 }
+            };
+            for (int i = 0; i < range; i++) 
+                for (int j = 0; j < range; j++)
+                    dataGridView1[i,j].Value = init[j,i];
+
+            //button2_Click(sender, e);
+
+         
+            //MessageBox.Show("");
+        }
+
+        void PrintMatrix(int[,] matrix, int[,]check)
+        {
+            if (rowIndexGlobal != 0)
+            { 
+                dataGridView2.Rows.Add();
+                rowIndexGlobal++;
+            }
+
+            for (int i = 0; i < range; i++)
+            {
+                dataGridView2.Rows.Add();
+                for (int j = 0; j < range; j++)
+                    dataGridView2[j, rowIndexGlobal + i].Value = matrix[i, j];
+            }
+
+            for (int i = 0; i < range; i++)
+                for (int j = 0; j < range; j++)
+                {
+                    if (check[i, j] == 1)
+                        dataGridView2[j, rowIndexGlobal + i].Style.ForeColor = Color.Blue;
+                    if (check[i, j] == -1)
+                        dataGridView2[j, rowIndexGlobal + i].Style.ForeColor = Color.Red;
+                }
+
+            rowIndexGlobal += range;
+
+        }
+        (int,List<int>) hungarian(int[,] matrix)
+        {
+            int[,] init = new int[range,range];
+            for (int i = 0; i < range; i++)
+                for (int j = 0; j < range; j++)
+                    init[i,j] = matrix[i,j];
+           
+            do
+            {
+                //Получение нулей в каждой строке
+                for (int i = 0; i < range; i++)
+                {
+                    int minIndex = 0;
+                    int minValue = matrix[i, 0];
+                    for (int j = 1; j < range; j++)
+                        if (matrix[i, j] < minValue)
+                        {
+                            minIndex = j;
+                            minValue = matrix[i, j];
+                        }
+                    for (int j = 0; j < range; j++)
+                        matrix[i, j] -= minValue;
+                }
+                //Получение нулей в каждом столбце
+                for (int i = 0; i < range; i++)
+                {
+                    int minIndex = 0;
+                    int minValue = matrix[0, i];
+                    for (int j = 1; j < range; j++)
+                        if (matrix[j, i] < minValue)
+                        {
+                            minIndex = j;
+                            minValue = matrix[j, i];
+                        }
+                    for (int j = 0; j < range; j++)
+                        matrix[j, i] -= minValue;
+                }
+
+                //Поиск оптимального решения
+                int[,] check = new int[range, range];
+                int minZeroIndex;
+                int minZeroCount;
+                int countZeroPl = 0;
+                do
+                {
+                    minZeroIndex = -1;
+                    minZeroCount = range;
+                    for (int i = 0; i < range; i++)
+                    {
+                        int zeroCount = 0;
+                        for (int j = 0; j < range; j++)
+                            if (matrix[i, j] == 0 && check[i, j] == 0)
+                                zeroCount++;
+
+                        if (zeroCount < minZeroCount && zeroCount > 0)
+                        {
+                            minZeroIndex = i;
+                            minZeroCount = zeroCount;
+                        }
+                            
+                    }
+                    bool first = true;
+                    if (minZeroIndex != -1)
+                    {
+                        countZeroPl++;
+                        for (int i = 0; i < range; i++)
+                            if (matrix[minZeroIndex, i] == 0 && check[minZeroIndex, i] == 0)
+                            {
+                                if (first)
+                                {
+                                    check[minZeroIndex, i] = 1;
+                                    for (int j = 0; j < range; j++)
+                                        if (matrix[j, i] == 0 && check[j, i] == 0)
+                                            check[j, i] = -1;
+                                    first = false;
+                                }
+                                else check[minZeroIndex, i] = -1;
+                            }
+                    }
+
+
+
+                } while (minZeroIndex != -1);
+                PrintMatrix(matrix,check);
+                if (countZeroPl == range)
+                {
+                    
+                    // Формирование результата
+                    List<int> result = new List<int>();
+                    for (int i = 0; i < range; i++)
+                        for (int j = 0; j < range; j++)
+                            if (check[i, j] == 1)
+                            {
+                                result.Add(j);
+                                break;
+                            }
+                    int sum = 0;
+                    for (int i = 0; i < range; i++)
+                        sum += init[i, result[i]];
+                    return (sum,result);
+                }
+                //return (0, null);
+                bool[] rows = new bool[range];
+                bool[] cols = new bool[range];
+                for (int i = 0; i < range; i++)
+                {
+                    rows[i] = true; cols[i] = true;
+                }
+                //1.1
+                for (int i = 0; i < range; i++)
+                {
+                    bool hasZero = false;
+                    for (int j = 0; j < range; j++)
+                        if (check[i, j] == 1)
+                        {
+                            hasZero = true;
+                            break;
+                        }
+                    rows[i] = hasZero;
+                }
+
+                //1.2
+                for (int i = 0; i < range; i++)
+                {
+                    if (!rows[i])
+                    {
+                        for (int j = 0; j < range; j++)
+                            if (check[i, j] == -1)
+                            {
+                                cols[j] = false;
+                                break;
+                            }
+
+                    }
+                }
+                for (int i = 0; i < range; i++)
+                    if (!cols[i])
+                        for (int j = 0; j < range; j++)
+                            if (check[j, i] == 1)
+                                rows[j] = false;
+                List<int> list = new List<int>();
+                for (int i = 0; i < range; i++)
+                    for (int j = 0; j < range; j++)
+                        if (!(rows[i] || !cols[j])&& matrix[i, j]!=0)
+                            list.Add(matrix[i, j]);
+                int min = list.Min();
+                for (int i = 0; i < range; i++)
+                    if (rows[i])
+                        for (int j = 0; j < range; j++)
+                            matrix[i, j] -= min;
+                for (int i = 0; i < range; i++)
+                    if (!cols[i])
+                        for (int j = 0; j < range; j++)
+                            matrix[j, i] += min;
+
+            } while (true);
+
+           
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear(); 
+            range = Convert.ToInt32(numericUpDown1.Value);
+            for (int i = 0; i < range; i++)
+                dataGridView1.Columns.Add(i+"", "");
+            for (int i = 0; i < range; i++)
+                dataGridView1.Rows.Add("");
+            for (int i = 0; i < range; i++)
+                for (int j = 0; j < range; j++)
+                    dataGridView1[i, j].Value = 0;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            for (int i = 0; i < range; i++)
+                for (int j = 0; j < range; j++)
+                    dataGridView1[i, j].Value = random.Next(100);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            rowIndexGlobal = 0;
+            dataGridView2.Rows.Clear();
+            dataGridView2.Columns.Clear();
+            for (int i = 0; i < range; i++)
+                dataGridView2.Columns.Add("" + i, "");
+            int[,] matrix = new int[range, range];
+            for (int i = 0;i < range; i++)
+                for(int j = 0;j < range; j++)
+                    matrix[i,j] = Convert.ToInt32(dataGridView1[j,i].Value);
+            int sum;
+            List<int> res;
+            (sum,res)= hungarian(matrix);
+            label2.Text = "результат " + sum;
+        }
+    }
+}
